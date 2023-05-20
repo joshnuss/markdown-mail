c855adce2f04b4dac72a33c91c9e6c20cf465bcc
Josh Nussbaum <joshnuss@gmail.com>
2023-05-19 22:33:53 -0400

    Revert "Prepare app for deployment"
    This reverts commit 2151ab9a77fefc0a13a8660f487fa463c07fa770.

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,46 +1,40 @@
 import { db } from '$lib/server/db'
 import { faker } from '@faker-js/faker'
 
-if (await db.subscriber.count() == 0) {
-  await setup()
-}
-
-async function setup() {
-  await db.subscriber.createMany({
-    data: Array.from({ length: 200 }).map(() => {
-      return {
-        email: faker.internet.email(),
-        firstName: faker.name.firstName(),
-        lastName: faker.name.lastName(),
-        status: 'SUBSCRIBED',
-        confirmationCode: crypto.randomUUID(),
-        unsubscribeCode: crypto.randomUUID(),
-      }
-    })
+await db.subscriber.createMany({
+  data: Array.from({ length: 200 }).map(() => {
+    return {
+      email: faker.internet.email(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      status: 'SUBSCRIBED',
+      confirmationCode: crypto.randomUUID(),
+      unsubscribeCode: crypto.randomUUID(),
+    }
   })
+})
 
-  const subjects = ['Welcom aboard!', '10 svelte tips']
-  const subscribers = await db.subscriber.findMany()
+const subjects = ['Welcom aboard!', '10 svelte tips']
+const subscribers = await db.subscriber.findMany()
 
-  for (let subject of subjects) {
-    const content = faker.lorem.paragraphs(4)
+for (let subject of subjects) {
+  const content = faker.lorem.paragraphs(4)
 
-    await db.broadcast.create({
-      data: {
-        subject,
-        content,
-        messages: {
-          create: subscribers.map(subscriber => ({
-            subject,
-            content,
-            subscriber: {
-              connect: {
-                id: subscriber.id
-              }
+  await db.broadcast.create({
+    data: {
+      subject,
+      content,
+      messages: {
+        create: subscribers.map(subscriber => ({
+          subject,
+          content,
+          subscriber: {
+            connect: {
+              id: subscriber.id
             }
-          }))
-        }
+          }
+        }))
       }
-    })
-  }
+    }
+  })
 }
